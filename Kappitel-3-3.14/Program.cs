6394ee2ff1466bf355379037f5d5d1584a93e7ac
JesperWintberg <111962418+JesperWintberg@users.noreply.github.com>
2022-09-27 19:26:26 +0200

    Add project files.

---
+++ Kappitel-3-3.14/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Globalization;
+
+namespace ____
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Har du gått ut Gymnasiet?");
+            Console.WriteLine("Svara med J eller N || J = ja  N = nej");
+            string skola = Console.ReadLine();
+            Console.WriteLine("Hur gammal är du?");
+            int ålder = int.Parse(Console.ReadLine());
+
+            if (skola == "J" && ålder < 22)
+            {
+                Console.WriteLine("Vi vill gärna anställa dig");
+            }
+
+            else
+            {
+                Console.WriteLine("Vi letar tyvärr efter\r\nannan personal just nu");
+            }
+        }
+    }
+}
